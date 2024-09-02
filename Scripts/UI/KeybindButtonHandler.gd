df0a49ac6d2b006260b7ea82270a8f1e775368bc
Sequoia Haynes <supersequoia11@gmail.com>
2024-09-02 09:50:35 -0700

    carson look i did something helpful

--- Scripts/UI/KeybindButtonHandler.gd
+++ Scripts/UI/KeybindButtonHandler.gd
@@ -21,6 +21,7 @@ func _input(event) -> void:
 			var key_string := DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
 			text = OS.get_keycode_string(key_string)
 			keybind_text = text
+			changeKeybind(self.get_meta("action"),self.get_meta("key_number"),keybind_text)
 		else:
 			keybind_changed.emit()
 		
@@ -41,13 +42,17 @@ func reloadKeybinds():
 	
 	Parse.parse(configText)
 	var parseData = Parse.data
-	for ygu in parseData["keybinds"].size():
-		for hbnnkjjijo in parseData["keybinds"][ygu]["key"].size():
+	
+	for y in parseData["keybinds"].size():
+		for x in parseData["keybinds"][y]["key"].size():
 			print(InputMap.get_actions())
-			add_input_action(parseData["keybinds"][ygu]["key"][hbnnkjjijo],parseData["keybinds"][ygu]["actionName"])
-	keybindConfig.close()
+			add_input_action(parseData["keybinds"][y]["key"][x],parseData["keybinds"][y]["actionName"])
 			
-func changeKeybind(actionName:String,keybindNumber:int,newKey:String)->bool:
+	keybindConfig.close()
+	
+#key number represents the index of the key in the action, if in an action has multiple keys
+#like for example up arrow and space for jump
+func changeKeybind(actionName:String,keyNumber:int,newKey:String)->bool:
 	#buffer_parse holds the json object that will hold the updated keybinds.json
 	var buffer_parse:JSON = JSON.new()
 	var lerawjson = FileAccess.open("user://keybinds.json",FileAccess.READ)
@@ -65,7 +70,7 @@ func changeKeybind(actionName:String,keybindNumber:int,newKey:String)->bool:
 			foundActionNumber = bhhbj
 			break
 			
-	#the action that this function was called to change doesnt exist, print out to avoid errors
+	#the action that this function was called to change doesnt exist, print out to avoid bugs
 	if(foundActionNumber == -1):
 		print("change_keybind was  just passed a nonexistent action, something is likely wrong")
 		return false
@@ -73,13 +78,14 @@ func changeKeybind(actionName:String,keybindNumber:int,newKey:String)->bool:
 	#no matter how many times i write a fix, this bug always comes back somehow
 	#update: for anyone who isnt me and doesnt remember what that comment was referring to
 	#the code below basically just does the same exact thing as the "hack" in UiGenerator.gd
-	if(keybindNumber>leJsonBuffer["keybinds"][foundActionNumber]["key"].size()):
+	if(keyNumber>leJsonBuffer["keybinds"][foundActionNumber]["key"].size()):
 		leJsonBuffer["keybinds"][foundActionNumber]["key"][0] = newKey
 	else:
-		leJsonBuffer["keybinds"][foundActionNumber]["key"][keybindNumber] = newKey
+		leJsonBuffer["keybinds"][foundActionNumber]["key"][keyNumber] = newKey
 		
 	lerawjson = FileAccess.open("user://keybinds.json",FileAccess.WRITE)
 	lerawjson.store_string(buffer_parse.stringify(leJsonBuffer,"\t"))
 	lerawjson.close()
-	get_parent().get_node("UIGenerator").reinitialize_ui()
+	get_parent().reinitialize_ui()
+	
 	return true
