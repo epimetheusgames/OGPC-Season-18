49944a2e9f2f7cb6ac6af26ba3b5228091522abe
Sequoia Haynes <supersequoia11@gmail.com>
2024-09-17 15:29:24 -0700

    il do it later guys (after i play 10 hrs of factorio)

--- Scripts/Skeleton/TextPosSet.gd
+++ Scripts/Skeleton/TextPosSet.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
 var dialog_played: bool = false
+var dialog:String = ""
+# In characters per second 
 var dialog_speed: float = 0
+
 var dialog_played_time: float = 0
 
-
+@onready var text_node = get_node("TextureRect")
 func _ready() -> void:
 	get_node("TextureRect").size = get_node("RichTextLabel").size*0.9
 	get_node("TextureRect").position = get_node("RichTextLabel")
-	
+	play_dialog("hi im a silly little dialog thingy doing dialog stuff look at me isnt that interesting",10)
 func _process(delta:float):
 	if(not dialog_played):
 		dialog_played_time += delta*60
+		text_node.text += dialog[int(dialog_played_time/dialog_speed)]
 		if(dialog_played_time>=dialog_speed):
 			dialog_played = true
 
-func play_dialog(dialog:String, dialog_speed_inp:int):
+func play_dialog(dialog_input:String, dialog_speed_inp:int):
 	dialog_played = false
+	dialog = dialog_input
 	dialog_speed = dialog_speed_inp
--- Scripts/UI/UiGenerator.gd
+++ Scripts/UI/UiGenerator.gd
@@ -56,10 +56,12 @@ func _ready():
 	if(not FileAccess.file_exists("user://keybinds.json")):
 		keybind_savefile.store_string(json_config_generator());
 		keybind_savefile.close();
-	var check_thingy = JSON.new().parse(json_config_generator())
-	var other_check_thingy = JSON.new().parse(keybind_savefile.get_as_text())
-	if(check_thingy.data["keybinds"].length>other_check_thingy.data["keybinds"].length):
-		pass
+	var check_thingy = JSON.new()
+	check_thingy.parse(json_config_generator())
+	var other_check_thingy = JSON.new()
+	other_check_thingy.parse(keybind_savefile.get_as_text())
+	if(check_thingy.data["keybinds"].size()>other_check_thingy.data["keybinds"].size()):
+		for i in other_check_thingy.data["key"]
 	reinitialize_ui()
 	
 func get_keybind_file():
@@ -101,8 +103,20 @@ func get_input_key_names(array: Array) -> Array:
 			var keycode := DisplayServer.keyboard_get_keycode_from_physical(array[x].physical_keycode)
 			return_array.append(OS.get_keycode_string(keycode))
 	else:
-		var keycode := DisplayServer.keyboard_get_keycode_from_physical(array[0].physical_keycode)
-		return_array.append(OS.get_keycode_string(keycode))
+		if(array[0] is InputEventMouseButton):
+			if(array[0].button_index == 1):
+				return_array.append("Left Click")
+			elif(array[0].button_index == 2):
+				return_array.append("Right Click")
+			elif(array[0].button_index == 3):
+				return_array.append("Middle Click")
+			elif(array[0].button_index == 4):
+				return_array.append("Mousewheel up")
+			elif(array[0].butto_index == 5):
+				return_array.append("Mousewheel down")
+		else:
+			var keycode := DisplayServer.keyboard_get_keycode_from_physical(array[0].physical_keycode)
+			return_array.append(OS.get_keycode_string(keycode))
 		
 	return return_array
 
