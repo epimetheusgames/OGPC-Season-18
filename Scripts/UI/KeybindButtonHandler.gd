f2fc5e1e24c9e00a3517252f2d4e9e7d78531e50
Carson Bates <blackhole243243@gmail.com>
2024-09-19 22:02:47 -0700

    Cleaned up KeybindButtonHandler

--- Scripts/UI/KeybindButtonHandler.gd
+++ Scripts/UI/KeybindButtonHandler.gd
@@ -1,8 +1,11 @@
+# TODO: FIX THIS GOOFY AHH CODE SEQUOIA!!!
+# TODO: I LITERALLY CANNOT UNDERSTAND WHAT IS BEING DONE HERE!!!
 extends Button
 
-@onready var keybind_text:String = text
-var setting_keybind:bool = false
-var Parse:JSON = JSON.new()
+
+@onready var keybind_text: String = text
+var setting_keybind := false
+var parse := JSON.new()
 
 signal keybind_changed
 
@@ -21,7 +24,7 @@ func _input(event) -> void:
 			var key_string := DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
 			text = OS.get_keycode_string(key_string)
 			keybind_text = text
-			changeKeybind(self.get_meta("action"),self.get_meta("key_number"),keybind_text)
+			change_keybind(get_meta("action"), get_meta("key_number"), keybind_text)
 		else:
 			keybind_changed.emit()
 		
@@ -40,52 +43,52 @@ func reloadKeybinds():
 	
 	var actions = get_parent().get_node("UIGenerator").remove_stock_keybinds(InputMap.get_actions())
 	
-	Parse.parse(configText)
-	var parseData = Parse.data
+	parse.parse(configText)
+	var parseData = parse.data
 	
 	for y in parseData["keybinds"].size():
 		for x in parseData["keybinds"][y]["key"].size():
-			print(InputMap.get_actions())
 			add_input_action(parseData["keybinds"][y]["key"][x],parseData["keybinds"][y]["actionName"])
 			
 	keybindConfig.close()
 	
 #key number represents the index of the key in the action, if in an action has multiple keys
 #like for example up arrow and space for jump
-func changeKeybind(actionName:String,keyNumber:int,newKey:String)->bool:
+# Returns if the function was successful ... probably not neccesary if we have the print statement?
+func change_keybind(action_name: String, key_number: int, new_key: String) -> bool:
 	#buffer_parse holds the json object that will hold the updated keybinds.json
 	var buffer_parse:JSON = JSON.new()
-	var lerawjson = FileAccess.open("user://keybinds.json",FileAccess.READ)
-	var lejson = lerawjson.get_as_text()
-	buffer_parse.parse(lejson)
+	var raw_json := FileAccess.open("user://keybinds.json",FileAccess.READ)
+	var json_text = raw_json.get_as_text()
+	buffer_parse.parse(json_text)
 	
-	var leJsonBuffer = buffer_parse.data
-	lerawjson.close()
+	var json_data = buffer_parse.data
+	json_text.close()
 	
-	#attempt to find the index of the named action in the keybinds[] array
-	var foundActionNumber = -1
-	for bhhbj in leJsonBuffer["keybinds"].size():
+	# Attempt to find the index of the named action in the keybinds array
+	var found_action_number := -1
+	for i in range(json_data["keybinds"].size()):
 		
-		if(leJsonBuffer["keybinds"][bhhbj]["actionName"]==actionName):
-			foundActionNumber = bhhbj
+		if json_data["keybinds"][i]["actionName"] == action_name:
+			found_action_number = i
 			break
 			
 	#the action that this function was called to change doesnt exist, print out to avoid bugs
-	if(foundActionNumber == -1):
-		print("change_keybind was  just passed a nonexistent action, something is likely wrong")
+	if found_action_number == -1:
+		print("WARNING: Nonexistant action passed to function change_keybind, this WILL cause errors.")
 		return false
 		
-	#no matter how many times i write a fix, this bug always comes back somehow
-	#update: for anyone who isnt me and doesnt remember what that comment was referring to
-	#the code below basically just does the same exact thing as the "hack" in UiGenerator.gd
-	if(keyNumber>leJsonBuffer["keybinds"][foundActionNumber]["key"].size()):
-		leJsonBuffer["keybinds"][foundActionNumber]["key"][0] = newKey
+	# That hack again!
+	if key_number > json_data["keybinds"][found_action_number]["key"].size():
+		json_data["keybinds"][found_action_number]["key"][0] = new_key
 	else:
-		leJsonBuffer["keybinds"][foundActionNumber]["key"][keyNumber] = newKey
-		
-	lerawjson = FileAccess.open("user://keybinds.json",FileAccess.WRITE)
-	lerawjson.store_string(buffer_parse.stringify(leJsonBuffer,"\t"))
-	lerawjson.close()
+		json_data["keybinds"][found_action_number]["key"][key_number] = new_key
+	
+	# Store data to file.
+	raw_json = FileAccess.open("user://keybinds.json", FileAccess.WRITE)
+	raw_json.store_string(buffer_parse.stringify(json_data, "\t"))
+	raw_json.close()
+	
 	get_parent().reinitialize_ui()
 	
 	return true
