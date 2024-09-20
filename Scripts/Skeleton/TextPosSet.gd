fe0c93a6ee3462282b3abb66c93378bf326c92fd
Sequoia Haynes <supersequoia11@gmail.com>
2024-09-20 15:32:18 -0700

    dialog go brbrbrbrbrbrb

--- Scripts/Skeleton/TextPosSet.gd
+++ Scripts/Skeleton/TextPosSet.gd
@@ -3,24 +3,41 @@ extends Node2D
 var dialog_played: bool = false
 var dialog:String = ""
 # In characters per second 
-var dialog_speed: float = 0
+var dialog_speed: float = 100
+
+# In characters per second, this is so theres a lil pause between a period
+#(as you might in real human speech)
+var dialog_speed_period: float = 1
 
 var dialog_played_time: float = 0
 
-@onready var text_node = get_node("TextureRect")
+var last_played_letterindex: int = 0
+@onready var text_node = get_node("DialogTextLabel")
 func _ready() -> void:
-	get_node("TextureRect").size = get_node("RichTextLabel").size*0.9
-	get_node("TextureRect").position = get_node("RichTextLabel")
-	play_dialog("hi im a silly little dialog thingy doing dialog stuff look at me isnt that interesting",10)
+	get_node("DialogTextLabel").size = get_node("DialogTextureBG").size*0.9
+	get_node("DialogTextLabel").position = get_node("DialogTextureBG").position
+	play_dialog("hi im a silly little dialog thingy. doing dialog stuff. look at me isnt that interesting",10)
 
 func _process(delta:float):
 	if(not dialog_played):
-		dialog_played_time += delta*60
-		text_node.text += dialog[int(dialog_played_time/dialog_speed)]
-		if(dialog_played_time>=dialog_speed):
+		if(not int(dialog_played_time/dialog_speed)==dialog.length()-1):
+			if(dialog[int(dialog_played_time/dialog_speed)+1]=="."):
+				dialog_played_time += delta*60*dialog_speed_period
+			else:
+				dialog_played_time += delta*60*dialog_speed
+		else:
+			dialog_played_time += delta*60*dialog_speed
+			
+		if(int(dialog_played_time/dialog_speed)!=last_played_letterindex):
+			if(int(dialog_played_time/dialog_speed)==dialog.length()):
+				text_node.text += dialog[dialog.length()-1]
+			else:
+				text_node.text += dialog[int(dialog_played_time/dialog_speed)]
+		if(dialog_played_time>=dialog_speed*dialog.length()-1):
 			dialog_played = true
-
+		last_played_letterindex = int(dialog_played_time/dialog_speed)
 func play_dialog(dialog_input:String, dialog_speed_inp:int):
 	dialog_played = false
 	dialog = dialog_input
 	dialog_speed = dialog_speed_inp
+	
--- Scripts/Skeleton/root.gd
+++ Scripts/Skeleton/root.gd
@@ -4,12 +4,14 @@ extends Node
 @export_node_path("Node") var game_skeleton_node
 @export_node_path("Control") var ui_root_node
 @export_node_path("Node") var game_root_node
+@export_node_path("Node2D") var dialog_text_node
 
 func _ready():
 	Global.root_node = self
 	Global.game_skeleton_node = get_node(game_skeleton_node)
 	Global.ui_root_node = get_node(ui_root_node)
 	Global.game_root_node = get_node(game_root_node)
+	Global.dialog_text_node = get_node(dialog_text_node)
 
 # I would reccomend not doing this ... you lose control over the game!
 # I will be using my SaveLoadFramework node for this.
