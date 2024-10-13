2657459700356f52278150506bf2999f0ca4fdb5
Carson Bates <blackhole243243@gmail.com>
2024-10-12 20:42:15 -0700

    Good stuff for tight spaces

--- Scripts/Entity/Diver/NewDiver.gd
+++ Scripts/Entity/Diver/NewDiver.gd
@@ -9,8 +9,14 @@ extends Entity
 func _physics_process(delta: float):
 	velocity = diver_movement.get_velocity()
 	
-	var target_angle: float = velocity.angle() + PI/2
-	
+	var target_angle: float
+	if !($Movement/RightWallRaycast.get_collider() && $Movement/LeftWallRaycast.get_collider()):
+		target_angle = velocity.angle() + PI/2
+	else:
+		# Fancy math to make the player do good stuff in tight spaces.
+		var forward_angle: float = $Movement/RightWallRaycast.get_collision_normal().angle() + PI
+		var offset_angle: float = clamp(angle_difference(rotation, velocity.angle() + PI / 2) * 0.3, -0.4, 0.4)
+		target_angle = forward_angle + offset_angle
 	var angle_diff: float = angle_difference(rotation, target_angle)
 	rotation += clamp(angle_diff * 0.1, -0.1, 0.1)
 	
