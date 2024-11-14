3301a041bef78238c032b5b827a81e144b9340f9
Carson Bates <blackhole243243@gmail.com>
2024-11-13 19:48:58 -0800

    Create AutoNavigationMeshGenDemo scene

---
+++ Scripts/Demos/AutoNavigationMeshGen.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+@onready var polygons: Array[CollisionPolygon2D] = [$NavigationMeshObject1/CollisionPolygon2D, $NavigationMeshObject2/CollisionPolygon2D]
+
+func _ready():
+	
+	for polygon in polygons:
+		var nav_polygon = Polygon2D.new()
+		nav_polygon.polygon = polygon.polygon
+		nav_polygon.position = polygon.global_position
+		nav_polygon.visible = false
+		$NavigationRegion2D.add_child(nav_polygon)
+	
+	$NavigationRegion2D.bake_navigation_polygon()
