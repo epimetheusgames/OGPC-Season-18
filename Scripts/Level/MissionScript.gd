74ff2581c74e8727e8e8c131bf68024d9ba4b103
Carson Bates <blackhole243243@gmail.com>
2025-01-18 16:47:50 -0800

    Revert "Merge Dialog"

--- Scripts/Level/MissionScript.gd
+++ Scripts/Level/MissionScript.gd
@@ -26,9 +26,3 @@ func _ready():
 		$LevelContainer/Level/Waves/Line2D7.material = null
 		$LevelContainer/Level/Background.material = null
 		$LevelContainer/Level/TransBackground.material = null
-	var overlay_ui = CanvasLayer.new()
-	overlay_ui.name = "overlay_ui"
-	overlay_ui.follow_viewport_enabled = false
-	self.add_child(overlay_ui)
-	for x in get_parent().get_parent().get_node("GameUIOverlay").get_children().size():
-		get_node("overlay_ui").add_child(get_parent().get_parent().get_node("GameUIOverlay").get_children()[x].duplicate())
