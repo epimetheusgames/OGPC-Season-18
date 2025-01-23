6ea4d7f65876a5c62c3b06242e2536721758485e
Kai Benson <kaitaobenson@gmail.com>
2025-01-23 11:15:22 -0800

    Change nothing

--- Scripts/Level/MissionScript.gd
+++ Scripts/Level/MissionScript.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 func turn_on_efficient():
 	if Global.super_efficient:
 		$LevelContainer/Level/Waves/Line2D.material = null
@@ -30,6 +29,9 @@ func _ready():
 		$LevelContainer/Level/Waves/Line2D7.material = null
 		$LevelContainer/Level/OceanShader/Background.material = null
 		$LevelContainer/Level/OceanShader/TransBackground.material = null
+	
+	# What the acrualy skib is this code
+	"""
 	var overlay_ui = CanvasLayer.new()
 	overlay_ui.name = "overlay_ui"
 	overlay_ui.follow_viewport_enabled = false 
@@ -37,3 +39,4 @@ func _ready():
 	for x in get_parent().get_parent().get_node("GameUIOverlay").get_children().size():
 		get_node("overlay_ui").add_child(get_parent().get_parent().get_node("GameUIOverlay").get_children()[x].duplicate())
 	get_parent().get_parent().get_parent().get_node("UI").get_node("MainMenu").visible = false
+	"""
