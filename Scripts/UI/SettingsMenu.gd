736cef7519529285c8ce49b1c4cd5d3ed31fe4ce
Sequoia Haynes <supersequoia11@gmail.com>
2025-04-27 16:21:40 -0700

    bad word

--- Scripts/UI/SettingsMenu.gd
+++ Scripts/UI/SettingsMenu.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var master_bus_db
+@onready var master_bus_db = AudioServer.get_bus_volume_db(0)
 @onready var MainContainer = get_node("ContainTheDumbShit")
 @onready var BackButton = MainContainer.get_node("BackButton")
 @onready var SoundButton = MainContainer.get_node("HBoxContainer/VBoxContainer/SoundButton")
@@ -19,12 +19,12 @@ func _process (_delta:float) -> void:
 		SoundButton.pressed.connect(_open_sound_button)
 		SoundMenuBackButton.pressed.connect(_close_sound_button)
 		master_bus_db = AudioServer.get_bus_volume_db(0)
-		SoundSlider.value_changed.connect(_master_volume_changed)
-		BrightnessSlider.value_changed.connect(func(): Global.set_brightness(BrightnessSlider.value))
 		HasCodeRan = true
 		
-func _master_volume_changed() -> void:
-	AudioServer.set_bus_volume_db(0,master_bus_db*SoundSlider.value/100)
+func _master_volume_changed(value) -> void:
+	AudioServer.set_bus_volume_linear(0,SoundSlider.value)
+func _brightness_slider_value_changed(value) -> void:
+	Global.set_brightness(BrightnessSlider.value)
 
 func _open_sound_button() -> void:
 	SoundMenuBackground.visible = true
