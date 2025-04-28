ee509151bb17450b7d808f007f7e381f239cdeba
Kai Benson <kaitaobenson@gmail.com>
2025-04-27 17:36:40 -0700

    stuff

--- Scripts/Entity/Diver/DiverStats.gd
+++ Scripts/Entity/Diver/DiverStats.gd
@@ -1,52 +1,38 @@
 class_name DiverStats
 extends Node2D
 
-var max_health: float = health
-var health: float = 100
-var oxygen_percentage: float = 100
-var oxygen_loss: float
-var completion_areas_entered: Array[CompletionArea]
+const max_oxygen: float = 100
+var oxygen: float = max_oxygen
+
+var oxygen_loss: float = 0.01
+var boost_oxygen_loss: float = 0.05
+
 var current_money: int = 0
+
 @onready var hurtbox: Hurtbox = $Hurtbox
 @onready var diver: Diver = get_parent()
 
-func _ready() -> void:
-	hurtbox.damaged.connect(_damaged)
-	
-	while true:
-		await get_tree().create_timer(0.8).timeout
-		if oxygen_percentage <= 0:
-			hurtbox.damage(5, null)
 
 func _process(delta: float) -> void:
 	if Global.godot_steam_abstraction && Global.is_multiplayer && !diver._is_node_owner():
 		return
 	
-	oxygen_percentage -= oxygen_loss * delta * 60
 	if diver.get_state() == Diver.DiverState.IN_GRAVITY_AREA:
-		oxygen_percentage = 100
-	if abs(diver.global_position.y) < 80:
-		oxygen_percentage = 100
+		oxygen = 100
 	
-	health = hurtbox.health
+	if diver.diver_movement.is_boosting:
+		oxygen -= boost_oxygen_loss * delta * 60
+	else:
+		oxygen -= oxygen_loss * delta * 60
+	
 	
 	if Global.godot_steam_abstraction:
 		Global.godot_steam_abstraction.sync_var(self, "health")
-		Global.godot_steam_abstraction.sync_var(self, "oxygen_percentage")
+		Global.godot_steam_abstraction.sync_var(self, "oxygen")
 
-func _damaged(damage_amount: float, by: Attackbox) -> void:
-	diver.camera.shake(2.5, 0.4)
-
-func _on_general_detection_box_area_entered(area: Area2D) -> void:
-	if area is CompletionArea:
-		completion_areas_entered.append(area)
-
-func _on_general_detection_box_area_exited(area: Area2D) -> void:
-	if area is CompletionArea:
-		completion_areas_entered.remove_at(completion_areas_entered.find(area))
 
 func get_health() -> float:
-	return health
+	return hurtbox.health
 
 func get_max_health() -> float:
-	return max_health
+	return hurtbox.max_health
--- Scripts/Objects/CompletionArea.gd
+++
@@ -1,4 +0,0 @@
-# Yes this should be empty. It's just so that scripts can identify that it's
-# this specific area.
-class_name CompletionArea
-extends Area2D
