9f120c0595e89a7d491ecb2fae665578e2e33237
Carson Bates <blackhole243243@gmail.com>
2025-05-03 11:59:34 -0700

    All three music tracks implemented

--- Scripts/Level/EnvironmentEffects.gd
+++ Scripts/Level/EnvironmentEffects.gd
@@ -1,12 +1,33 @@
 class_name EnvironmentEffects
 extends Node
 
+enum Playing {
+	HAPPY,
+	SCARY,
+	SUBMARINE,
+}
+
+var playing: Playing = Playing.SUBMARINE
+
 var master_bus := AudioServer.get_bus_index("Master")
 var sfx_bus := AudioServer.get_bus_index("SFX")
 var music_bus := AudioServer.get_bus_index("Music")
-var wind_bus := AudioServer.get_bus_index("Wind")
 
-func _process(_delta: float) -> void:
-	pass
-	#var low_pass_effect: AudioEffectLowPassFilter = AudioServer.get_bus_effect(wind_bus, 0)
-	#low_pass_effect.cutoff_hz = max(1500 - abs(Global.player.position.y) * 2, 50)
+@onready var animator: AnimationPlayer = $MusicFade
+
+func _ready() -> void:
+	animator.play("RESET")
+
+func _process(delta: float) -> void:
+	if !Global.player.diver_movement.is_in_gravity_area && playing == Playing.SUBMARINE:
+		animator.play("SubmarineToHappy")
+		playing = Playing.HAPPY
+	if Global.player.get_oxygen() < 55 && playing == Playing.HAPPY:
+		animator.play("HappyToScary")
+		playing = Playing.SCARY
+	if Global.player.diver_movement.is_in_gravity_area && playing == Playing.HAPPY:
+		animator.play("HappyToSubmarine")
+		playing = Playing.SUBMARINE
+	if Global.player.diver_movement.is_in_gravity_area && playing == Playing.SCARY:
+		animator.play("ScaryToSubmarine")
+		playing = Playing.SUBMARINE
