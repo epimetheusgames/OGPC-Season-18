022aa67241e4ff59a6789ed87d036dfbacacf397
Xavier Jin <xavierbunbunny@gmail.com>
2025-05-04 18:22:06 -0700

    Submarine hatch and rough implementation of flipping

---
+++ Scripts/Entity/Submarine/HatchPivot.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var hatch
+@onready var hatch_navigation
+
+var players_inside : int = 0
+
+@export var max_rotation : float = -PI * 8 / 9
+@export var rotation_rate : float = .5
+
+func set_node_variables() -> void:
+	hatch = $"../Hatch"
+	hatch_navigation = $"../NavigationObstacle/Hatch"
+
+func _process(delta: float) -> void:
+	if players_inside >= 1:
+		rotation = Util.better_angle_lerp(rotation, max_rotation, rotation_rate, delta)
+		hatch.rotation = Util.better_angle_lerp(hatch.rotation, max_rotation, rotation_rate, delta)
+		hatch_navigation.rotation = Util.better_angle_lerp(hatch_navigation.rotation, max_rotation, rotation_rate, delta)
+	else:
+		rotation = Util.better_angle_lerp(rotation, 0.0, rotation_rate, delta)
+		hatch.rotation = Util.better_angle_lerp(hatch.rotation, 0.0, rotation_rate, delta)
+		hatch_navigation.rotation = Util.better_angle_lerp(hatch_navigation.rotation, 0.0, rotation_rate, delta)
+
+func _on_hatch_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player_area"):
+		players_inside += 1
+
+func _on_hatch_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player_area"):
+		players_inside -= 1
